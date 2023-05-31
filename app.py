230afae5408e93fb8bb32722d46f87185b89d749
Kagev <kaplitskiygv@gmail.com>
2023-06-01 00:52:29 +0300

    0.0.2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import Adafruit_BBIO.GPIO as GPIO
+import os
 
 app = Flask(__name__)
 
@@ -8,9 +9,11 @@ led_pin = "P9_14"
 # Set the LED pin as an output
 GPIO.setup(led_pin, GPIO.OUT)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/toggle_led/<state>')
 def toggle_led(state):
@@ -20,6 +23,12 @@ def toggle_led(state):
         GPIO.output(led_pin, GPIO.LOW)
     return state
 
+
+# Get the current working directory
+current_directory = os.getcwd()
+
+# Print the current working directory
+print("Current Working Directory:", current_directory)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8888)
-    
